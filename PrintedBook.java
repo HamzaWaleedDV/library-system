56acec44b76970b02b7397f7c6eff3e5909be132
AyoubElashryDV <datagamer201@gmail.com>
2026-03-12 23:31:52 +0200

    added the User and PrintedBook files

---
+++ PrintedBook.java
@@ -0,0 +1,13 @@
+
+public class PrintedBook {
+
+    public int pages;
+
+    public PrintedBook(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPages() {
+        return this.pages;
+    }
+}
